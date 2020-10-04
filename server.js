cf0b1dffa8f6c685126d0f6919ef6e8fb8e7347f
ktatianacastillo <kimberlyt.castillo@gmail.com>
2020-10-03 21:11:45 -0600

    update text

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const PORT = 80;
 const HOST = '0.0.0.0';
 
 const app = express();
-app.get('/:numero', (req, res) => {
-  const numero = req.params.numero;
-  if (Number.isInteger(numero * 2)) {
-    res.send(`Your number is ${evenOrOdd(numero) ? 'EVEN' : 'ODD'}`);
+app.get('/:number', (req, res) => {
+  const number = req.params.number;
+  if (Number.isInteger(number * 2)) {
+    res.send(`Your number (${number}) is ${evenOrOdd(number) ? 'EVEN' : 'ODD'}`);
   } else {
     res.send("The param must be number type");
   }
